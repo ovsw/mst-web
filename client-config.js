64b10f490332721022bdfa4293102968d16d4d61
Ovi Savescu <ovi@ovswebsites.com>
2020-05-05 14:39:19 +0300

    updated sanity id

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'zf88zcuk',
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '0tskkacc',
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
