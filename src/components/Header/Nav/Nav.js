fb6e97ee125ab445b6a03c5c3ea0e99a10ce48ed
Ovi Savescu <ovi@ovswebsites.com>
2020-05-16 08:50:41 +0300

    fixed link URLs in menu

--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -37,7 +37,7 @@ const Nav = ({isMobileNavOpen, closeMobileNav}) => {
           <ul className='top-nav__outer-list' id='menu-main-menu'>
             {menuToShow.items.map(({title, link: {content: {main: {slug}}}, subItems = []}) => (
               <li key={slug.current} className={`top-nav__item-level-1 menu-item ${subItems.length > 0 ? 'top-nav__has-children' : ''}`}>
-                <Link to={slug.current} onClick={() => closeMobileNav()}>{title}</Link>
+                <Link to={`/${slug.current}/`} onClick={() => closeMobileNav()}>{title}</Link>
                 {(subItems.length > 0) && <SubMenu subItems={subItems} closeMobileNav={closeMobileNav} />}
               </li>
             ))}
