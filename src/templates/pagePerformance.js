11200f1455d85032756f913ab59d745a5d833074
Ovi Savescu <ovi@ovswebsites.com>
2020-05-18 21:15:37 +0300

    added actual template code

--- src/templates/pagePerformance.js
+++ src/templates/pagePerformance.js
@@ -1,17 +1,25 @@
 /** @jsx jsx */
 import {jsx} from 'theme-ui'
 import React, {useEffect} from 'react' // eslint-disable-line
+import SEO from '../components/seo'
+import RenderModules from '../utils/renderModules'
 // import {graphql, Link} from 'gatsby'
 
-const Page = () => {
+const Page = ({path, pageContext}) => {
+  // console.log(path, pageContext)
+
+  const {
+    main: {
+      modules,
+      slug
+    },
+    meta
+  } = pageContext
+
   return (
     <>
-      <p>this is a performance page</p>
-      <p>this is a performance page</p>
-      <p>this is a performance page</p>
-      <p>this is a performance page</p>
-      <p>this is a performance page</p>
-      <p>this is a performance page</p>
+      <SEO metaInfo={meta} pagePath={slug.current} />
+      {RenderModules(modules)}
     </>
   )
 }
