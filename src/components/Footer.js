c321ac482e1f2a57a106efcbea6dc8165ba0b91c
Ovi Savescu <ovi@ovswebsites.com>
2020-05-18 11:39:44 +0300

    remoed comment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,12 +50,6 @@ const Footer = () => {
       </footer>
     </div>
   )
-
-  // <footer sx={{borderTop: '1px solid', borderColor: 'gray.5', py: 3}}>
-  //   <Container sx={{variant: ['utils.textCenter']}}>
-  //     <p>Copyright &copy;{new Date().getFullYear()} <a href='https://timisoaratourguide.com' target='_blank' rel='noopener noreferrer'>Timisoara Tour Guide</a>, All Rights Reserved. | Website by <a href='https://ovswebsites.com' target='_blank' rel='noopener noreferrer'>OVS Websites</a> </p>
-  //   </Container>
-  // </footer>
 }
 
 export default Footer
