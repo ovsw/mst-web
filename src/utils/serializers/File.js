0ef06c35267679a11de8e1a58ea6af74147bb403
Ovi Savescu <ovi@ovswebsites.com>
2020-06-05 12:36:13 +0300

    added file serializer

--- src/utils/serializers/File.js
+++ src/utils/serializers/File.js
@@ -4,8 +4,8 @@ import {jsx} from 'theme-ui'
 import {FaFileAlt} from 'react-icons/fa'
 
 export default ({node}) => {
-  // console.log(node)
+  console.log(node)
   return (
-    <a href={node.asset.url} rel='noopener noreferrer' className='contentButton' target='_blank'><FaFileAlt sx={{position: 'relative', top: '2px'}} />  {node.description}</a>
+    <a href={node.file.asset.url} rel='noopener noreferrer' className='contentButton' target='_blank'><FaFileAlt sx={{position: 'relative', top: '2px'}} />  {node.description}</a>
   )
 }
