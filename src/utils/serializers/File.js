1970631e596c5ad8ba2b681606f824e45ba9e8fc
Ovi Savescu <ovi@ovswebsites.com>
2020-06-05 18:56:33 +0300

    performance directory

--- src/utils/serializers/File.js
+++ src/utils/serializers/File.js
@@ -4,7 +4,6 @@ import {jsx} from 'theme-ui'
 import {FaFileAlt} from 'react-icons/fa'
 
 export default ({node}) => {
-  console.log(node)
   return (
     <a href={node.file.asset.url} rel='noopener noreferrer' className='contentButton' target='_blank'><FaFileAlt sx={{position: 'relative', top: '2px'}} />  {node.description}</a>
   )
