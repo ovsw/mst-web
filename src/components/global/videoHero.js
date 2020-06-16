3dbc006d1426ca6cb65e3b45a89ea60bd2b59c35
Ovi Savescu <ovi@ovswebsites.com>
2020-06-16 17:32:49 +0300

    switched to css module for video hero

--- src/components/global/videoHero.js
+++ src/components/global/videoHero.js
@@ -5,27 +5,27 @@ import BlockContent from '@sanity/block-content-to-react'
 import serializers from '../../utils/serializers'
 import {Link} from 'gatsby'
 
-import './videoHero.css'
+import s from './videoHero.module.css'
 
 const VideoHero = ({data}) => {
-  const {buttonText, caption, image, text, title, url, video} = data
+  const {buttonText, text, title, url, video} = data
   const [courtainClass, setCourtainClass] = useState('')
   useEffect(() => {
     setCourtainClass('open')
   })
   let videoSource = null
   let posterBg = null
-  let heroType = 'performances'
+  let heroType = 'Performances'
   switch (video) {
     case 'performances1' :
       videoSource = 'https://mstvideo-ac97.kxcdn.com/mainstages-intro-montage.mp4'
       posterBg = '/images/hero-bg-live.jpg'
-      heroType = 'performances'
+      heroType = 'Performances'
       break
     case 'live1' :
       videoSource = 'https://lrw-ac97.kxcdn.com/mainstages-families-bg-video.mp4'
       posterBg = '/images/hero-bg-live.jpg'
-      heroType = 'live'
+      heroType = 'Live'
       break
     default :
       videoSource = 'https://mstvideo-ac97.kxcdn.com/mainstages-intro-montage.mp4'
@@ -33,27 +33,25 @@ const VideoHero = ({data}) => {
 
   return (
     <section
-      className={`hero hero--${heroType}`}
+      id={`hero--${heroType}`}
+      className={`${s.hero} ${s[`hero${heroType}`]}`}
     >
-      <video className='hero__video-bg' playsInline='' autoPlay muted loop poster={posterBg} id='heroVideo'>
+      <video className={s.hero__videoBg} playsInline='' autoPlay muted loop poster={posterBg} id='heroVideo'>
         <source data-src={videoSource} type='video/mp4' src={videoSource} />
-        {/* http://odcv-ac97.kxcdn.com/oasis-intro-short-hq.mp4  */}
-        {/* <source src="http://odcv-ac97.kxcdn.com/oasis-intro-short-hq.ogv" type="video/ogg"/>
-        <source src="http://odcv-ac97.kxcdn.com/oasis-intro-short-hq.webm" type="video/webm">  */}
       </video>
 
-      <div className='hero__content'>
-        <Styled.h1 className='hero__heading' sx={{color: 'primary'}}>{title}</Styled.h1>
-        <div className='hero__hookline'><BlockContent blocks={text} serializers={serializers} /></div>
+      <div className={s.hero__content}>
+        <Styled.h1 className={s.hero__heading} sx={{color: 'primary'}}>{title}</Styled.h1>
+        <div className={s.hero__hookline}><BlockContent blocks={text} serializers={serializers} /></div>
         <Link
-          className='hero__cta' to={url} sx={{
-            variant: 'buttons.simpleWhite'
+          className={s.hero__cta} to={url} sx={{
+            variant: 'buttons.simple'
           }}
         >{buttonText}
         </Link>
       </div>
-      <img className={`hero__left-curtain ${courtainClass}`} src='/images/left-courtain.png' />
-      <img className={`hero__right-curtain ${courtainClass}`} src='/images/right-courtain.png' />
+      <img className={`${s.hero__leftCurtain} ${courtainClass}`} src='/images/left-courtain.png' />
+      <img className={`${s.hero__rightCurtain} ${courtainClass}`} src='/images/right-courtain.png' />
     </section>
   )
 }
