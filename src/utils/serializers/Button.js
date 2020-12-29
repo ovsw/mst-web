eb198fe8c3e17730d6ad76c1281e516bf150b0e5
Ovi Savescu <ovi@ovswebsites.com>
2020-12-29 08:52:00 +0200

    made button handle absolute URLs properly

--- src/utils/serializers/Button.js
+++ src/utils/serializers/Button.js
@@ -2,12 +2,15 @@
 import {jsx} from 'theme-ui'
 import React from 'react' // eslint-disable-line
 import {Link} from 'gatsby'
+import isAbsoluteURL from 'is-absolute-url'
 import {FaChevronRight} from 'react-icons/fa'
 
 export default ({node}) => {
   return (
     <div sx={{textAlign: 'left', mt: 3}}>
-      <Link to={node.url} className='contentButton'>{node.buttonText} <FaChevronRight sx={{position: 'relative', top: '0.25rem'}} /></Link>
+      {isAbsoluteURL(node.url)
+        ? (<a href={node.url} className='contentButton' target='_blank'>{node.buttonText} <FaChevronRight sx={{position: 'relative', top: '0.25rem'}} /></a>)
+        : (<Link to={node.url} className='contentButton'>{node.buttonText} <FaChevronRight sx={{position: 'relative', top: '0.25rem'}} /></Link>)}
     </div>
   )
 }
